b7af0fe1a332a701840fd572e523e685163deb0d
Eduardo Mejias <mejiasavilaeduardo@gmailcom>
2018-10-13 22:47:17 -0400

    add cambios desafio3

--- Desafio 3 JS1/js/desafio3js1.js
+++ Desafio 3 JS1/js/desafio3js1.js
@@ -1,8 +1,22 @@
 var a=0;
 var b=0;
+var aa=0;
+var ab=0;
+var ba=0;
+var bb=0;
+var bc=0;
+var bd=0;
+var be=0;
+var c=0;
+var ca=0;
+var cb=0;
+var da,db,dc,de,df,dg = 0;
+
+
 
 function desafio1_1(){
-	n=Math.ceil(Math.random()*5);
+	n=Math.ceil(Math.random()*3);
+	console.log("n: " +n);
 	for(i=1;i<=n;i++){
 		console.log("i: ",i);
 		funa: do{
@@ -26,24 +40,65 @@ function desafio1_1(){
 						else
 							a=a+1;
 		}while(a<1)
-		var num=Math.random()*20;
-		var subtotal=Math.random()*200000;
-		var valor_total_ven=Math.random()*1000000;
-		comision=valor_total_ven*0.1
+		funaa: do{
+			var num=parseInt(prompt("Ingrese el numero de ventas que hizo " +nombre+ " :"));
+			if(num==null){
+				if(confirm("Esta seguro que desea salir?")){
+				alert("HASTA PRONTO");
+				console.log(cossto); //uso esta instruccion para que se cierre la compilacion.
+				}
+				else
+					continue funaa;
+			}
+			else
+				if(num==""){
+					alert("No debes dejar la casilla vacia");
+					continue funaa;
+				}
+				else
+					if(isNaN(num))
+						alert("Debe escribir un nombre real, no letras\n Usted escribio: "+num);
+						else
+							aa=aa+1;
+		}while(aa<1)	
+		var subtotal=num*200000;
+		funab: do{
+			var valor_total=parseFloat(prompt("Ingrese el valor total de ventas que hizo " +nombre+ " :"));
+			if(valor_total==null){
+				if(confirm("Esta seguro que desea salir?")){
+				alert("HASTA PRONTO");
+				console.log(cossto); //uso esta instruccion para que se cierre la compilacion.
+				}
+				else
+					continue funab;
+			}
+			else
+				if(valor_total==""){
+					alert("No debes dejar la casilla vacia");
+					continue funab;
+				}
+				else
+					if(isNaN(valor_total))
+						alert("Debe escribir un nombre real, no letras\n Usted escribio: "+valor_total);
+						else
+							ab=ab+1;
+		}while(ab<1)	
+		comision=valor_total*0.1
 		total=500000+subtotal+comision;
 		console.log("nombre: " +nombre);
 		console.log("numero de autos vendidos: " +num);
 		console.log("Comision por autos vendidos: " +subtotal);
-		console.log("Valor total de sus ventas: " +valor_total_ven);
+		console.log("Valor total de sus ventas: " +valor_total);
 		console.log("Comision por el total de sus ventas: " +comision);
 		console.log("total :" +total)
-		alert("el salario neto de " +nombre+" es "+total);
+		alert("el salario neto de " +nombre+" es "+total+ " BsS");
+
 	}
 }
 
 function desafio1_2(){
 	funb: do{
-		n=prompt("Ingrese el numero de estudiantes a los que se le evaluara el promedio:");
+		n=parseInt(prompt("Ingrese el numero de estudiantes a los que se le evaluara el promedio:"));
 		console.log("numero de estudiantes: "+n);
 		if(n==null){
 			if(confirm("Esta seguro que desea salir?")){
@@ -65,11 +120,145 @@ function desafio1_2(){
 						b=b+1;
 	}while(b<1)
 	for(i=1;i<=n;i++){
-		var nota1=Math.random()*27;
-		var nota2=Math.random()*27;
-		var nota3=Math.random()*27;
-		var evalcont=Math.random()*9;
-		var trabpract=Math.random()*10;
+		funba: do{
+			nota1=parseFloat(prompt("Ingrese la nota del segundo examen parcial (x/27):"));
+			console.log("nota 1 : "+nota1);
+			if(nota1<0 || nota1>27){
+				alert("Debes intriducir una nota entre 0 y 27");
+				continue funba;
+			}
+			else
+				if(nota1==null){
+					if(confirm("Esta seguro que desea salir?")){
+						alert("HASTA PRONTO");
+						console.log(cosssto);//Instruccion para generar error y hacer el break.
+					}
+					else
+						continue funba;
+				}
+				else
+					if(nota1==""){
+						alert("No debes dejar la casilla vacia");
+						continue funba;
+					}
+					else
+						if(isNaN(nota1)){
+							alert("Debes introducir un Numero");
+							continue funba;
+						}
+						else
+							ba=ba+1;
+		}while(ba<1)
+		fb: do{
+			nota2=parseFloat(prompt("Ingrese la nota del segundo examen parcial (x/27):"));
+			console.log("nota 2 : "+nota2);
+			if(nota2<0 || nota2>27){
+							alert("Debes intriducir una nota entre 0 y 27");
+							continue fb;
+			}
+			else
+				if(nota2==null){
+					if(confirm("Esta seguro que desea salir?")){
+						alert("HASTA PRONTO");
+						console.log(cosssto);//Instruccion para generar error y hacer el break.
+					}
+					else
+						continue fb;
+				}
+				else
+					if(nota2==""){
+						alert("No debes dejar la casilla vacia");
+						continue fb;
+					}
+					else
+						if(isNaN(nota2)){
+							alert("Debes introducir un Numero");
+							continue fb;
+						}
+						else
+							bb=bb+1;		
+		}while(bb<1)
+		funbc: do{
+			nota3=parseFloat(prompt("Ingrese la nota del segundo examen parcial (x/27):"));
+			console.log("nota 3 : "+nota3);
+			if(nota3<0 || nota3>27){
+				alert("Debes intriducir una nota entre 0 y 27");
+				continue funbc;
+			}
+				else
+					if(nota3==null){
+						if(confirm("Esta seguro que desea salir?")){
+							alert("HASTA PRONTO");
+							console.log(cosssto);//Instruccion para generar error y hacer el break.
+						}
+						else
+							continue funbc;
+					}
+					else
+						if(nota3==""){
+							alert("No debes dejar la casilla vacia");
+							continue funbc;
+						}
+						else
+							if(isNaN(nota3))
+								alert("Debes introducir un Numero");
+								else
+									bc=bc+1;
+		}while(bc<1)
+		funbd: do{
+			evalcont=parseFloat(prompt("Ingrese la nota del segundo examen parcial (x/9):"));
+			console.log("evaluacion continua : "+evalcont);
+			if(evalcont<0 || evalcont>9){
+				alert("Debes intriducir una nota entre 0 y 9");
+				continue funbd;
+			}
+				else
+					if(evalcont==null){
+						if(confirm("Esta seguro que desea salir?")){
+							alert("HASTA PRONTO");
+							console.log(cosssto);//Instruccion para generar error y hacer el break.
+						}
+						else
+							continue funbd;
+					}
+					else
+						if(evalcont==""){
+							alert("No debes dejar la casilla vacia");
+							continue funbd;
+						}
+						else
+							if(isNaN(evalcont))
+								alert("Debes introducir un Numero");
+								else
+									bd=bd+1;
+		}while(bd<1)
+		funbe: do{
+			trabpract=parseFloat(prompt("Ingrese la nota del segundo examen parcial (x/10):"));
+			console.log("trabajo practico : "+trabpract);
+			if(trabpract<0 || trabpract>10){
+				alert("Debes introducir una nota entre 0 y 10");
+				continue funbe;
+			}
+				else
+					if(trabpract==null){
+						if(confirm("Esta seguro que desea salir?")){
+							alert("HASTA PRONTO");
+							console.log(cosssto);//Instruccion para generar error y hacer el break.
+						}
+						else
+							continue funbe;
+					}
+					else
+						if(trabpract==""){
+							alert("No debes dejar la casilla vacia");
+							continue funbe;
+						}
+						else
+							if(isNaN(trabpract))
+								alert("Debes introducir un Numero");
+								else
+									be=be+1;
+		}while(be<1)
 		notafinal=nota1+nota2+nota3+evalcont+trabpract;
 		base20=(notafinal*20)/100;
 		base4=(notafinal*4)/100;
@@ -78,9 +267,246 @@ function desafio1_2(){
 		console.log(nota3)
 		console.log(evalcont)
 		console.log(trabpract)
-		console.log(notafinal)
-		console.log(base20)
-		console.log(base4)
-		alert("la nota final del estudiante en escala de 100pts es "+notafinal+"la nota en escala de 20pts es"+base20+'y la nota en base 4 es'+base4);
+		console.log("nota final base 100: " +notafinal)
+		console.log("nota final base 20: " +base20)
+		console.log("nota final base 4: " +base4)
+		alert("la nota final del estudiante en escala de 100pts es " +notafinal+ " la nota en escala de 20pts es "+base20+ " y la nota en base 4 es " +base4);
 	}	
+}
+
+function desafio1_3(){
+	func: do{
+		n=parseInt(prompt("Ingrese el numero de clientes a los que se les hara sus calculos:"));
+		console.log("numero de estudiantes: "+n);
+		if(n==null){
+			if(confirm("Esta seguro que desea salir?")){
+				alert("HASTA PRONTO");
+				console.log(cosssto);//Instruccion para generar error y hacer el break.
+			}
+			else
+				continue func;
+		}
+		else
+			if(n==""){
+				alert("No debes dejar la casilla vacia");
+				continue func;
+			}
+			else
+				if(isNaN(n))
+					alert("Debes introducir un Numero");
+					else
+						c=c+1;
+	}while(c<1)
+	for(i=1;i<=n;i++){
+		funca: do{
+			var cliente=prompt("Nombre del cliente " +n+ " :");
+			if(cliente==null){
+					if(confirm("Esta seguro que desea salir?")){
+						alert("HASTA PRONTO");
+						console.log(costo); //uso esta instruccion para que se cierre la compilacion.
+					}
+					else
+						continue funca;
+			}
+			else
+				if(cliente==""){
+					alert("No debes dejar la casilla vacia");
+					continue funca;
+				}
+				else
+					if(!isNaN(cliente))
+						alert("Debe escribir un nombre real, no numeros\n Usted escribio: "+cliente);
+						else
+							ca=ca+1;
+		}while(ca<1)
+		funcb: do{
+			costo=parseFloat(prompt("Ingrese el precio total del vehiculo:"));
+			console.log("cosot : " +costo);
+			if(costo==null){
+				if(confirm("Esta seguro que desea salir?")){
+					alert("HASTA PRONTO");
+					console.log(cosssto);//Instruccion para generar error y hacer el break.
+				}
+				else
+					continue funcb;
+			}
+			else
+				if(costo==""){
+					alert("No debes dejar la casilla vacia");
+					continue funcb;
+				}
+				else
+					if(isNaN(costo))
+						alert("Debes introducir un Numero");
+						else
+							cb=cb+1;
+		}while(cb<1)
+		inicial=costo*0.3;
+		resto=(costo-inicial)/24;
+		console.log("cliente: " +cliente)
+		console.log("costo: " +costo)
+		console.log("inicial: " +inicial)
+		console.log("resto a pagar: " +resto)
+		alert("El cliente: "+cliente+" tendra una cuota inicial del vehiculo igual de: "+inicial+" y cuotas mensuales por 2 anos de: "+resto);
+	}
+}
+
+function desafio1_4(){
+	n=Math.ceil(Math.random()*3);
+	console.log("n: " +n);
+	for(i=1;i<=n;i++){
+		funda: do{
+			des=parseFloat(prompt("Ingrese el costo del desayuno:"));
+			console.log("costo del desayuno : " +des);
+			if(des==null){
+				if(confirm("Esta seguro que desea salir?")){
+					alert("HASTA PRONTO");
+					console.log(cosssto);//Instruccion para generar error y hacer el break.
+				}
+				else
+					continue funda;
+			}
+			else
+				if(des==""){
+					alert("No debes dejar la casilla vacia");
+					continue funda;
+				}
+				else
+					if(isNaN(des))
+						alert("Debes introducir un Numero");
+						else
+							da=da+1;
+		}while(da<1)
+		fundb: do{
+			numd=parseFloat(prompt("Ingrese el numero de desayunos vendidos:"));
+			console.log("num desay vendidos : " +numd);
+			if(numd==null){
+				if(confirm("Esta seguro que desea salir?")){
+					alert("HASTA PRONTO");
+					console.log(cosssto);//Instruccion para generar error y hacer el break.
+				}
+				else
+					continue fundb;
+			}
+			else
+				if(numd==""){
+					alert("No debes dejar la casilla vacia");
+					continue fundb;
+				}
+				else
+					if(isNaN(numd))
+						alert("Debes introducir un Numero");
+						else
+							db=db+1;
+		}while(db<1)
+		fundc: do{
+			alm=parseFloat(prompt("Ingrese el costo del almuerzo:"));
+			console.log("costo almuerzo : " +alm);
+			if(alm==null){
+				if(confirm("Esta seguro que desea salir?")){
+					alert("HASTA PRONTO");
+					console.log(cosssto);//Instruccion para generar error y hacer el break.
+				}
+				else
+					continue fundc;
+			}
+			else
+				if(alm==""){
+					alert("No debes dejar la casilla vacia");
+					continue fundc;
+				}
+				else
+					if(isNaN(alm))
+						alert("Debes introducir un Numero");
+						else
+							dc=dc+1;
+		}while(dc<1)
+		funde: do{
+			numa=parseFloat(prompt("Ingrese el numero de almuerzos vendidos:"));
+			console.log("num almuerz vendidos : " +numa);
+			if(numa==null){
+				if(confirm("Esta seguro que desea salir?")){
+					alert("HASTA PRONTO");
+					console.log(cosssto);//Instruccion para generar error y hacer el break.
+				}
+				else
+					continue funde;
+			}
+			else
+				if(numa==""){
+					alert("No debes dejar la casilla vacia");
+					continue funde;
+				}
+				else
+					if(isNaN(numa))
+						alert("Debes introducir un Numero");
+						else
+							de=de+1;
+		}while(de<1)
+		fundf: do{
+			cen=parseFloat(prompt("Ingrese el costo de la cena:"));
+			console.log("cosoto cena : " +cen);
+			if(cen==null){
+				if(confirm("Esta seguro que desea salir?")){
+					alert("HASTA PRONTO");
+					console.log(cosssto);//Instruccion para generar error y hacer el break.
+				}
+				else
+					continue fundf;
+			}
+			else
+				if(cen==""){
+					alert("No debes dejar la casilla vacia");
+					continue fundf;
+				}
+				else
+					if(isNaN(cen))
+						alert("Debes introducir un Numero");
+						else
+							df=df+1;
+		}while(df<1)
+		fundg: do{
+			numc=parseFloat(prompt("Ingrese el numero de cenas vendidos:"));
+			console.log("num cenas vendidos : " +numc);
+			if(numc==null){
+				if(confirm("Esta seguro que desea salir?")){
+					alert("HASTA PRONTO");
+					console.log(cosssto);//Instruccion para generar error y hacer el break.
+				}
+				else
+					continue fundg;
+			}
+			else
+				if(numc==""){
+					alert("No debes dejar la casilla vacia");
+					continue fundg;
+				}
+				else
+					if(isNaN(numc))
+						alert("Debes introducir un Numero");
+						else
+							dg=dg+1;
+		}while(dg<1)
+		ingd=des*numd;
+		inga=alm*numa;
+		ingc=cen*numc;
+		total=ingd+inga+ingc;
+		prom=(des+alm+cen)/3;
+		console.log(des);
+		console.log(numd);
+		console.log(alm);
+		console.log(numa);
+		console.log(cen);
+		console.log(numc);
+		console.log("ingreso desayuno: " +ingd);
+		console.log("ingreso almuerzo: " +inga);
+		console.log("ingreso cena: " +ingc);
+		console.log("total: " +total);
+		console.log("promedio: " +prom);
+		alert("El ingreso por desayunos es: "+ingd+" BsS");
+		alert("El ingreso por almuerzos es: "+inga+" BsS");
+		alert("El ingreso por cenas es: "+ingc+" BsS");
+		alert("El total de ingreso del dia es de: "+total+" BsS");
+		alert("El promedio de los precios de las comidas es de: "+prom);
+	}
 }
