f0b0026438eaf5859f16e06191a6b3add90fb446
avilapro <mejiasavilaeduardo@gmail.com>
2020-01-29 14:49:26 -0400

    cambios de clase1 js1 	new file:   Clase 1 JS/clase1.rar 	modified:   Clase 1 JS/clase1_2.html 	new file:   Clase 1 JS/src/clase1_2.js

---
+++ Clase 1 JS/src/clase1_2.js
@@ -0,0 +1,12 @@
+    console.log("x:"+x);
+	var x,y,z=5;
+	console.log("x:"+x);
+	x=1000;
+	console.log("x:"+x);
+	x++;
+	y+=10;
+	w=z+--x
+	console.log("x:"+ x);
+	console.log("y:"+ y);
+	console.log("z:"+ z);
+	console.log("w:"+ w);
